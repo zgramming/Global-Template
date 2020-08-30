71f0453c980a94434942d3fffa3ed75da8adea25
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-08-30 11:20:32 +0700

    first commit

---
+++ lib/variable/config/app_config.dart
@@ -0,0 +1,14 @@
+class AppConfig {
+  final String indonesiaLocale = 'id_ID';
+
+  static const urlImageAsset = 'asset/images';
+  static const defaultImageNetwork = 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png';
+
+  static const headerFont = 'LexendDeca';
+
+  static const MENU_DOSEN = 'menu_dosen';
+  static const MENU_PELAJARAN = 'menu_pelajaran';
+  static const MENU_TUGAS = 'menu_tugas';
+}
+
+final appConfig = AppConfig();
