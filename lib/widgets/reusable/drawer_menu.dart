518e518b53f6d039fedcf92b5630969c1ee82d14
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-10-31 11:35:59 +0700

    Re-work drawer menu widget

--- lib/widgets/reusable/drawer_menu.dart
+++ lib/widgets/reusable/drawer_menu.dart
@@ -1,54 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:global_template/global_template.dart';
 
 class DrawerMenu extends StatelessWidget {
-  final Function onTap;
-  final Color color;
-  final String menuName;
+  final VoidCallback onTap;
+  final String title;
   final IconData icon;
+  final bool showDivider;
   DrawerMenu({
+    this.title = 'Menu',
+    this.icon = Icons.home,
+    this.showDivider = false,
     this.onTap,
-    this.icon = Icons.subscriptions,
-    this.color = Colors.black,
-    this.menuName = 'Beranda',
   });
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: BoxConstraints(
-        maxWidth: sizes.width(context),
-      ),
-      child: InkWell(
-        onTap: onTap,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 5.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Flexible(
-                flex: 2,
-                fit: FlexFit.tight,
-                child: Icon(
-                  icon,
-                  size: sizes.width(context) / 14,
-                  color: color,
-                ),
-              ),
-              Flexible(
-                flex: 10,
-                fit: FlexFit.tight,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 24.0),
-                  child: Text(
-                    menuName,
-                    style: TextStyle(fontSize: 14, color: color),
-                  ),
-                ),
-              ),
-            ],
-          ),
+    return Column(
+      children: [
+        ListTile(
+          onTap: onTap,
+          title: Text(title),
+          trailing: Icon(icon),
         ),
-      ),
+        if (showDivider) Divider(),
+      ],
     );
   }
 }
