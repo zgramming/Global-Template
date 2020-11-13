f14dd68fa0eb6970a7cefc18aba0f8e115c88557
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-11-13 16:38:19 +0700

    add loading builder

--- lib/widgets/reusable/show_image_network.dart
+++ lib/widgets/reusable/show_image_network.dart
@@ -6,11 +6,12 @@ class ShowImageNetwork extends StatelessWidget {
   const ShowImageNetwork({
     @required this.imageUrl,
     this.imageCircleRadius = 35,
-    this.imageRadius = 10,
+    this.imageBorderRadius = 10,
     @required this.imageSize,
     this.isCircle = false,
     this.padding = const EdgeInsets.all(0),
     this.alignment = Alignment.center,
+    this.loadingBuilder,
     this.fit,
     this.onErrorImage,
   });
@@ -26,8 +27,8 @@ class ShowImageNetwork extends StatelessWidget {
   ///! Setting Image Circle Radius
   final double imageCircleRadius;
 
-  ///! Setting Image Circle Radius
-  final double imageRadius;
+  ///! Setting Image Border Radius
+  final double imageBorderRadius;
 
   ///! Setting Padding Image
   final EdgeInsetsGeometry padding;
@@ -41,22 +42,24 @@ class ShowImageNetwork extends StatelessWidget {
   ///! Handle Error Image
   final Function(BuildContext context, String url, dynamic error) onErrorImage;
 
+  ///! Handle when image loading
+  final Widget Function(BuildContext, String) loadingBuilder;
   @override
   Widget build(BuildContext context) {
     final image = Padding(
       padding: padding,
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(imageRadius),
+        borderRadius: BorderRadius.circular(imageBorderRadius),
         child: CachedNetworkImage(
           imageUrl: imageUrl ?? 'https://flutter.dev/images/catalog-widget-placeholder.png',
-          height: sizes.height(context) / (imageSize ?? double.infinity),
-          width: sizes.width(context) / (imageSize ?? double.infinity),
+          height: sizes.height(context) / (imageSize ?? 1),
+          width: sizes.width(context) / (imageSize ?? 1),
           errorWidget: onErrorImage ??
               (BuildContext context, String url, dynamic error) {
                 return Center(
                     child: IconButton(icon: const Icon(Icons.error), onPressed: () => ''));
               },
-          placeholder: (context, url) => const CircularProgressIndicator(),
+          placeholder: loadingBuilder,
           fit: fit,
           alignment: alignment,
         ),
@@ -68,7 +71,7 @@ class ShowImageNetwork extends StatelessWidget {
             padding: padding,
             child: CircleAvatar(
               backgroundColor: Colors.transparent,
-              radius: imageCircleRadius,
+              radius: sizes.width(context) / imageCircleRadius,
               child: Container(
                 decoration: BoxDecoration(
                   shape: BoxShape.circle,
