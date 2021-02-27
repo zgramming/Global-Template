7fab3b2fdbafc22de209d40fb9de2171f5f423df
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-02-27 13:03:24 +0700

    .of(context) to .maybeOf(context) for null safety

--- lib/variable/sizes/sizes.dart
+++ lib/variable/sizes/sizes.dart
@@ -5,12 +5,12 @@ enum TypeLayout { mobile, largeMobile, tablet, laptop, desktop }
 const mobileSize = 600, largeMobileSize = 768, tabletSize = 992, laptopSize = 1200;
 
 class SizesDevice {
-  MediaQueryData mediaQuery(BuildContext context) => MediaQuery.of(context);
+  MediaQueryData mediaQuery(BuildContext context) => MediaQuery.maybeOf(context);
 
-  double width(BuildContext context) => MediaQuery.of(context).size.width;
-  double height(BuildContext context) => MediaQuery.of(context).size.height;
+  double width(BuildContext context) => MediaQuery.maybeOf(context).size.width;
+  double height(BuildContext context) => MediaQuery.maybeOf(context).size.height;
 
-  double statusBarHeight(BuildContext context) => MediaQuery.of(context).padding.top;
+  double statusBarHeight(BuildContext context) => MediaQuery.maybeOf(context).padding.top;
 
   double screenHeightMinusAppBar(BuildContext context) => sizes.height(context) - kToolbarHeight;
 
@@ -20,9 +20,9 @@ class SizesDevice {
   double screenHeightMinusAppBarMinusStatusBar(BuildContext context) =>
       sizes.height(context) - kToolbarHeight - statusBarHeight(context);
 
-  double keyboardHeight(BuildContext context) => MediaQuery.of(context).viewInsets.bottom;
+  double keyboardHeight(BuildContext context) => MediaQuery.maybeOf(context).viewInsets.bottom;
 
-  // double smallestDimension(BuildContext context) => MediaQuery.of(context).size.shortestSide;
+  // double smallestDimension(BuildContext context) => MediaQuery.maybeOf(context).size.shortestSide;
   MediaQueryData smallestDimension(BuildContext context) =>
       MediaQueryData.fromWindow(WidgetsBinding.instance.window);
 
@@ -46,18 +46,18 @@ class SizesDevice {
 
   double responsiveSize(BuildContext context) => width(context) / 100;
 
-  double dp4(BuildContext context) => MediaQuery.of(context).size.width / 100;
-  double dp6(BuildContext context) => MediaQuery.of(context).size.width / 70;
-  double dp8(BuildContext context) => MediaQuery.of(context).size.width / 54;
-  double dp10(BuildContext context) => MediaQuery.of(context).size.width / 41;
-  double dp12(BuildContext context) => MediaQuery.of(context).size.width / 34;
-  double dp14(BuildContext context) => MediaQuery.of(context).size.width / 29;
-  double dp16(BuildContext context) => MediaQuery.of(context).size.width / 26;
-  double dp18(BuildContext context) => MediaQuery.of(context).size.width / 23;
-  double dp20(BuildContext context) => MediaQuery.of(context).size.width / 20;
-  double dp22(BuildContext context) => MediaQuery.of(context).size.width / 17;
-  double dp24(BuildContext context) => MediaQuery.of(context).size.width / 16;
-  double dp25(BuildContext context) => MediaQuery.of(context).size.width / 15;
+  double dp4(BuildContext context) => MediaQuery.maybeOf(context).size.width / 100;
+  double dp6(BuildContext context) => MediaQuery.maybeOf(context).size.width / 70;
+  double dp8(BuildContext context) => MediaQuery.maybeOf(context).size.width / 54;
+  double dp10(BuildContext context) => MediaQuery.maybeOf(context).size.width / 41;
+  double dp12(BuildContext context) => MediaQuery.maybeOf(context).size.width / 34;
+  double dp14(BuildContext context) => MediaQuery.maybeOf(context).size.width / 29;
+  double dp16(BuildContext context) => MediaQuery.maybeOf(context).size.width / 26;
+  double dp18(BuildContext context) => MediaQuery.maybeOf(context).size.width / 23;
+  double dp20(BuildContext context) => MediaQuery.maybeOf(context).size.width / 20;
+  double dp22(BuildContext context) => MediaQuery.maybeOf(context).size.width / 17;
+  double dp24(BuildContext context) => MediaQuery.maybeOf(context).size.width / 16;
+  double dp25(BuildContext context) => MediaQuery.maybeOf(context).size.width / 15;
 }
 
 final sizes = SizesDevice();
